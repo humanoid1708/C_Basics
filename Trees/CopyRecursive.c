278a28d998f99277a309612a1a444fae3792bdbf
Ajay Kaushal <166790908+humanoid1708@users.noreply.github.com>
2025-01-17 19:22:46 +0530

    Commit for Trees

---
+++ Trees/CopyRecursive.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+void CreationNR(struct Node* root) {
+    char start = 'Y';
+    do {
+        struct Node* temp = root;
+        struct Node* curr = (struct Node*)malloc(sizeof(struct Node));
+        int new;
+        printf("Enter the data:");
+        scanf("%d", &new);
+        curr->data = new;
+        curr->left = NULL;
+        curr->right = NULL;
+        int flag = 0;
+        while(flag == 0) {
+            char ch;
+            printf("Enter 'l' for left or 'r' for right or 'q' to exit: ");
+            scanf(" %c", &ch);
+            if(ch == 'l') {
+                if(temp->left == NULL) {
+                    temp->left = curr;
+                    flag = 1;
+                }
+                temp = temp->left;
+            }
+            else {
+                if(ch == 'r') {
+                    if(temp->right == NULL) {
+                        temp->right = curr;
+                        flag = 1;
+                    }
+                temp = temp->right;
+                }
+            }
+        }
+        printf("Do you want to continue? (Y/N):");
+        scanf(" %c", &start);
+    } while(start == 'Y');
+    
+}
+
+struct Node* copyR(struct Node* root) {
+    struct Node* temp = NULL;
+    if(root != NULL) {
+        struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+        temp->data = root->data;
+        temp->left = copyR(root->left);
+        temp->right = copyR(root->right);
+    }
+    return temp;
+}
+
+struct Node* stack[100];
+int top = -1;
+
+int isEmpty() {
+    return top == -1;
+}
+
+void push(struct Node* node) {
+    stack[++top] = node;
+}
+
+struct Node* pop() {
+    if(isEmpty()) {
+        return NULL;
+    }
+    struct Node* node = stack[top--];
+    return node;
+
+}
+
+void inroderNR(struct Node* root) {
+    struct Node* temp = root;
+    while(1) {
+        while(temp != NULL) {
+            push(temp);
+            temp = temp->left;
+        }
+        if(isEmpty()) {
+            break;
+        }
+        temp = pop();
+        printf("%d ", temp->data);
+        temp = temp->right;
+    }
+}
+
+int main() {
+    struct Node* root = (struct Node*)malloc(sizeof(struct Node));
+    root->data = 1;
+    root->left = NULL;
+    root->right = NULL;
+    CreationNR(root);
+    struct Node* two = copyR(root);
+    //inroderNR(root);
+    inroderNR(two);
+    return 0;
+}
